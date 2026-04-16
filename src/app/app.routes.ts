26a68d571a3a94c17035bb019b42654a6fa19909
Jenssel Angelo <20239174@s.ubaguio.edu>
2026-04-16 12:12:36 +0800

    Added routing to our file
    Co-authored-by: Jonald Alea <20171948@s.ubaguio.edu>
    Co-authored-by: Jenssel Piluden <20239174@s.ubaguio.edu>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';;
+import { SettingsComponent } from './Components/settings/settings.component';
+import { GraphComponent } from './Components/graph/graph.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+  { path: 'dashboard', component: DashboardComponent },
+
+  { path: 'settings', component: SettingsComponent },
+
+  { path: 'graph', component: GraphComponent },
+
+  { path: '**', redirectTo: '/dashboard' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
